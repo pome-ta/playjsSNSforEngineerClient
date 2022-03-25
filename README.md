a24ac1556edd82b9a71f38eb7827ac81c9b21ebc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-25 13:31:17 +0900

    get post

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # playjsSNSforEngineerClient
+
+
+[https://pome-ta.github.io/playjsSNSforEngineerClient/](https://pome-ta.github.io/playjsSNSforEngineerClient/)
+
+
+[エンジニア・プログラマにしか使えないSNSを作ってみた話](https://qiita.com/HawkClaws/items/599d7666f55e79ef7f56)
+
+
+
--- docs/js/app.js
+++ docs/js/app.js
@@ -4,3 +4,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded');
 });
+
+
+const url = 'https://versatileapi.herokuapp.com/api/text/all?$orderby=_created_at%20desc&$limit=20'
+
+fetch(url)
+  .then(res => res.json())
+  .then((json_data) => {
+    console.log(json_data);
+  });
