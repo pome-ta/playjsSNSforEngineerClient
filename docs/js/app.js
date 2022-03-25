34dc9b3e9983f8ef94b7c64467b90030eff3460a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-03-25 16:53:00 +0900

    output

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,10 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+async function res_json(uri) {
+  const res = await fetch(uri);
+  const json_data = await res.json();
+  return json_data;
+}
+
+
+function createElementAddClass(tag, ...names) {
+  const element_obj = document.createElement(tag);
+  for (const name of names){
+    element_obj.classList.add(name);
+  }
+  return element_obj;
+}
+
+function outputResult(data) {
+  const pTag = createElementAddClass('p', 'out');
+  pTag.innerHTML = data;
+  document.body.appendChild(pTag);
+}
+
+
+
 const url = 'https://versatileapi.herokuapp.com/api/text/all?$orderby=_created_at%20desc&$limit=20'
 
-fetch(url)
-  .then(res => res.json())
-  .then((json_data) => {
-    console.log(json_data);
-  });
+const json_data = await res_json(url);
+outputResult(json_data);
